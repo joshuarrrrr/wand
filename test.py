8e08f4d881c466b51932ac098c8ef0dc9f681317
Joshua Reibert <joshua.reibert@uni-weimar.de>
2021-05-07 14:54:00 +0200

    Add script to test the library

---
+++ test.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+import evdev
+
+import wand
+
+REQUIRED_MT_CODES = [
+    evdev.ecodes.ABS_MT_SLOT,
+    evdev.ecodes.ABS_MT_POSITION_X,
+    evdev.ecodes.ABS_MT_POSITION_Y,
+    evdev.ecodes.ABS_MT_TRACKING_ID,
+]
+
+
+def is_multitouch_device(device):
+    capabilities = device.capabilities()
+    if not evdev.ecodes.EV_ABS in capabilities:
+        return False
+
+    abs_codes = [cap[0] for cap in capabilities[evdev.ecodes.EV_ABS]]
+    return all([code in abs_codes for code in REQUIRED_MT_CODES])
+
+
+def find_touch_device():
+    devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
+    return None if not devices else devices[0]
+
+
+if __name__ == "__main__":
+    touch_dev = find_touch_device()
+    if not touch_dev:
+        raise RuntimeError("No suitable multi-touch device found!")
+    print(touch_dev.name)
+
+    mt_device = wand.MultitouchDevice(path=touch_dev.path)
+    mt_device.start()
+
+    try:
+        while True:
+            new, updated, finished = mt_device.poll_events()
+
+            for touch in new:
+                print(f"new touch point {touch.id}")
+
+            for touch in updated:
+                print(f"updated: {touch.id} ({touch.x, touch.y})")
+
+            for touch in finished:
+                print(f"touch point {touch.id} was finished")
+    except KeyboardInterrupt:
+        print("Exiting...")
